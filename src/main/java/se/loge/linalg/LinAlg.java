44cc60de72fa6dfcec3e372ba27a908c55192e43
Lorenz Gerber <lorenzottogerber@gmail.com>
2018-01-12 12:27:07 +0100

    using apache common CSV

--- src/main/java/se/loge/linalg/LinAlg.java
+++ src/main/java/se/loge/linalg/LinAlg.java
@@ -1,8 +1,14 @@
 package se.loge.linalg;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 public class LinAlg {
   public static void main(String args[]){
@@ -18,20 +24,35 @@ public class LinAlg {
 
 		//Get file from resources folder
 		ClassLoader classLoader = getClass().getClassLoader();
-		File file = new File(classLoader.getResource(fileName).getFile());
-
-		try (Scanner scanner = new Scanner(file)) {
-
-			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				result.append(line).append("\n");
-			}
-
-			scanner.close();
-
+		InputStream stream = classLoader.getResourceAsStream(fileName);
+		InputStreamReader streamReader = new InputStreamReader(stream);
+		BufferedReader reader = new BufferedReader(streamReader);
+		
+		Iterable<CSVRecord> records = null;
+		
+		try {
+			records = CSVFormat.TDF.parse(reader);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		int numberColumns = records.iterator().next().size();
+		
+		for (CSVRecord record : records) {
+			for(int i = 0; i < numberColumns; i++) {
+				System.out.println(record.get(i));
+			}
+			
+		}
+
+		Scanner scanner = new Scanner(reader);
+
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			result.append(line).append("\n");
+		}
+
+		scanner.close();
+
 
 		return result.toString();
 	  
